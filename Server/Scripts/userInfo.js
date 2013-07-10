39e36ef2cb3276770962426ef7bbbad9077417bc
Simon Colmer <simon_colmer@hotmail.com>
2013-07-10 16:59:00 +0100

    Added anonymous username default

--- Server/Scripts/userInfo.js
+++ Server/Scripts/userInfo.js
@@ -1,6 +1,6 @@
 function UserInfo() {
     
-    var displayName = prompt('Enter your name:', '');
+    var displayName = prompt('Enter your name:', '') || 'Anonymous';
 
     return {
         displayName: displayName
