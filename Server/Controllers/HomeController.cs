ee4c0ec28d7418b04364a049b75a0df1e4c60921
Simon Colmer <simon_colmer@hotmail.com>
2013-10-11 23:52:23 +0100

    Added last build date on game

--- Server/Controllers/HomeController.cs
+++ Server/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Server.Models;
 
 namespace Server.Controllers
 {
     public class HomeController : Controller
     {
-         public ActionResult Index()
-         {
-             AsteroidsViewModel model = new AsteroidsViewModel();
+        private DateTime? _lastBuild;
 
-             return View(model);
-         }
+        public ActionResult Index()
+        {
+            if (!_lastBuild.HasValue)
+            {
+                System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                System.IO.FileInfo fileInfo = new System.IO.FileInfo(assembly.Location);
+                _lastBuild = fileInfo.LastWriteTime;
+            }
+
+            AsteroidsViewModel model = new AsteroidsViewModel(_lastBuild.Value);
+
+            return View(model);
+        }
     }
 }
--- Server/Models/AsteroidsViewModel.cs
+++ Server/Models/AsteroidsViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace Server.Models
+﻿using System;
+
+namespace Server.Models
 {
     public class AsteroidsViewModel
     {
+        public DateTime LastBuild { get; set; }
 
+        public AsteroidsViewModel(DateTime lastBuild)
+        {
+            LastBuild = lastBuild;
+        }
     }
 }
